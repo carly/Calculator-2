ecc2c87bcfdefde4c45c8e957231f90c7cc460ca
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 17:56:40 -0700

    Calc 2 end of day

--- calculator.py
+++ calculator.py
@@ -9,3 +9,51 @@ from arithmetic import *
 
 
 # Your code goes here
+while True: 
+    entry = raw_input("> ")
+    entry_new = entry.lstrip()
+    tokens = entry_new.split(" ")
+
+    operator = tokens[0]
+    num1 = int(tokens[1])
+    
+    if len(tokens) > 2:
+        num2 = int(tokens[2])
+    else:
+        num2 = None    
+
+
+    if operator == "q":
+        file.close()
+
+    elif operator == "+":
+        print add(num1, num2)
+
+    elif operator == "-":
+        print subtract(num1, num2)
+
+    elif operator == "*":
+        print multiply(num1, num2)
+
+    elif operator == "/":
+        print divide(num1, num2)
+
+    elif operator == "square":
+        print square(num1)
+
+    elif operator == "cube":
+        print cube(num1)
+
+    elif operator == "pow":
+        print power(num1, num2)
+
+    elif operator == "mod":
+        print mod(num1, num2)
+
+    else: 
+        print "Invalid entry. Try again."
+
+
+
+
+
